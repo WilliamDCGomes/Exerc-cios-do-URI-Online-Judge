395696418eb9fa75b50b0502237cc67114820d21
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-24 20:34:27 -0300

    Feitos em C Sharp

---
+++ Feitos em C Sharp/2453.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace ConsoleApp7 {
+    class Program {
+        static void Main(string[] args) {
+			string[] phrase = Console.ReadLine().Split();
+			int size = phrase.Length;
+			int aux=1;
+			for (int i = 0; i < size; i++) {
+				int subSize = phrase[i].Length;
+				string[] subString = new string[subSize];
+				for (int j = 0; j < subSize; j++) {
+					subString[j] = phrase[i].Substring(j, 1);
+					if (j == aux) {
+						Console.Write(subString[j]);
+						aux += 2;
+					}
+				}
+				if (i != size - 1) {
+					Console.Write(" ");
+				}
+				aux = 1;
+			}
+			Console.WriteLine();
+		}
+    }
+}
---
+++ Feitos em C Sharp/2454.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp8 {
+    class Program {
+        static void Main(string[] args) {
+			string[] vet = Console.ReadLine().Split();
+			int p = int.Parse(vet[0]); 
+			int r = int.Parse(vet[1]);
+			if (p == 0) {
+				Console.WriteLine("C");
+			}
+			else if (r == 0) {
+				Console.WriteLine("B");
+			}
+			else {
+				Console.WriteLine("A");
+			}
+		}
+    }
+}
