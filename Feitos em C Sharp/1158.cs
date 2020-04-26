03706896aaf900ec0b183d2d6cf4e2705252b024
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-26 20:57:50 -0300

    Feitos em C Sharp

---
+++ Feitos em C Sharp/1158.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp15 {
+    class Program {
+        static void Main(string[] args) {
+			int soma = 0;
+			int vezes = int.Parse(Console.ReadLine());
+			for (int z = 0; z < vezes; z++) {
+				string[] vet = Console.ReadLine().Split();
+				int x = int.Parse(vet[0]);
+				int y = int.Parse(vet[1]);
+				if (x % 2 == 0) {
+					x++;
+					for (int i = 0; i < y; i++) {
+						soma += x;
+						x += 2;
+					}
+				}
+				else {
+					for (int i = 0; i < y; i++) {
+						soma += x;
+						x += 2;
+					}
+				}
+				Console.WriteLine(soma);
+				soma = 0;
+			}
+		}
+    }
+}
---
+++ Feitos em C Sharp/2581.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ConsoleApp14 {
+    class Program {
+        static void Main(string[] args) {
+            int a = int.Parse(Console.ReadLine());
+            for (int z = 0; z < a; z++) {
+                string[] vet = Console.ReadLine().Split();
+                Console.WriteLine("I am Toorg!");
+            }
+        }
+    }
+}
