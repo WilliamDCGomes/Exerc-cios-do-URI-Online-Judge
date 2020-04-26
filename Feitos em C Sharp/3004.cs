b17748d8448bc290a65d877454500adadd35f4b5
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-26 20:02:19 -0300

    Feitos em C Sharp

---
+++ Feitos em C Sharp/3004.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace ConsoleApp13 {
+    class Program {
+        static void Main(string[] args) {
+			int times = int.Parse(Console.ReadLine());
+			for (int i = 0; i < times; i++) {
+				string[] vet = Console.ReadLine().Split();
+				int insideX = int.Parse(vet[0]);
+				int insideY = int.Parse(vet[1]);
+				int mainX = int.Parse(vet[2]);
+				int mainY = int.Parse(vet[3]);
+				if ((insideX < mainX && insideY < mainY) || (insideX < mainY && insideY < mainX)) {
+					Console.WriteLine("S");
+				}
+				else {
+					Console.WriteLine("N");
+				}
+			}
+		}
+    }
+}
---
+++ Feitos em C Sharp/3037.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace ConsoleApp12 {
+    class Program {
+        static void Main(string[] args) {
+			int times = int.Parse(Console.ReadLine());
+			for (int i = 0; i < times; i++) {
+				int quantJoao = 0, quantMaria = 0;
+				for (int j = 0; j < 3; j++) {
+					string[] vet = Console.ReadLine().Split();
+					int x = int.Parse(vet[0]);
+					int d = int.Parse(vet[1]);
+					quantJoao += (x * d);
+				}
+				for (int j = 0; j < 3; j++) {
+					string[] vet = Console.ReadLine().Split();
+					int x = int.Parse(vet[0]);
+					int d = int.Parse(vet[1]);
+					quantMaria += (x * d);
+				}
+				if (quantJoao > quantMaria) {
+					Console.WriteLine("JOAO");
+				}
+				else {
+					Console.WriteLine("MARIA");
+				}
+			}
+		}
+    }
+}
