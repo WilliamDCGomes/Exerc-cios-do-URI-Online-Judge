e2a53a970015fe4bf1a3cc5fcf675ab51bc6570c
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-27 21:23:23 -0300

    Feitos em C Sharp

---
+++ Feitos em C Sharp/2540.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace ConsoleApp16 {
+    class Program {
+        static void Main(string[] args) {
+            while (true) {
+                string times = Console.ReadLine();
+                if (string.IsNullOrEmpty(times)) {
+                    break;
+                }
+                string[] imput = Console.ReadLine().Split();
+                int sizeInput = imput.Length;
+                int[] vet = new int[sizeInput];
+                int impeachment = 0;
+                for(int i = 0; i < sizeInput; i++) {
+                    vet[i] = int.Parse(imput[i]);
+                    if (vet[i] == 1) {
+                        impeachment++;
+                    }
+                }
+                double n = (double) (sizeInput * 2) / 3;
+                if (impeachment >=  n) {
+                    Console.WriteLine("impeachment");
+                }
+                else {
+                    Console.WriteLine("acusacao arquivada");
+                }
+            }
+        }
+    }
+}
