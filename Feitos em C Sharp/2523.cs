80e903a3359a8be1caf17cb498296439d5e5d496
William Douglas <57199250+WilliamDCGomes@users.noreply.github.com>
2020-04-30 19:45:49 -0300

    Feitos em C Sharp

---
+++ Feitos em C Sharp/2523.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ConsoleApp17 {
+    class Program {
+        static void Main(string[] args) {
+			while (true) {
+				string caracters = Console.ReadLine();
+				if (string.IsNullOrEmpty(caracters)) {
+					break;
+				}
+				int lamps = int.Parse(Console.ReadLine());
+				string[] onLamps = Console.ReadLine().Split();
+				for (int i = 0; i < lamps; i++) {
+					Console.Write(caracters.Substring(int.Parse(onLamps[i])-1,1));
+				}
+				Console.WriteLine();
+			}
+		}
+    }
+}
